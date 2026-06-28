c5d4201fb555eed5e0cfbf57d64baac91531c422
neutry <luisangel20.las@gmail.com>
2026-06-28 22:37:51 +0200

    pvdu: Embed dirwalker binary instead of pvdu-scanner

--- cmd/pvdu/main.go
+++ cmd/pvdu/main.go
@@ -8,11 +8,11 @@ import (
 	"github.com/neutry/pvdu/internal/k8s"
 )
 
-//go:embed pvdu-scanner
+//go:embed dirwalker
 var scannerBin embed.FS
 
 func main() {
-	data, err := scannerBin.ReadFile("pvdu-scanner")
+	data, err := scannerBin.ReadFile("dirwalker")
 	if err == nil {
 		k8s.ScannerBinary = data
 	}
